b72699a944d8863bf7c00bf30a30b1bd26698c78
kjuiop <copying_y@naver.com>
2025-03-29 20:07:05 +0900

    chapter06 : tftp 파일 업로드

--- chapter06/jake/tftp/server.go
+++ chapter06/jake/tftp/server.go
@@ -11,8 +11,13 @@ import (
 // Server represents a read-only TFTP server that supports a subset of
 // RFC 1350.
 type Server struct {
-	Payload []byte        // the payload served for all read requests
-	Retries uint8         // the number of times to retry a failed transmission
+	// 모든 읽기 요청에 반환될 페이로드
+	Payload []byte // the payload served for all read requests
+
+	// 전송 실패 시 재시도 횟수
+	Retries uint8 // the number of times to retry a failed transmission
+
+	// 전송 승인을 기다릴 시간
 	Timeout time.Duration // the duration to wait for an acknowledgment
 }
 
@@ -28,6 +33,7 @@ func (s Server) ListenAndServe(addr string) error {
 	return s.Serve(conn)
 }
 
+// Serve 서버 데이터 수신
 func (s *Server) Serve(conn net.PacketConn) error {
 	if conn == nil {
 		return errors.New("nil connection")
@@ -68,6 +74,7 @@ func (s *Server) Serve(conn net.PacketConn) error {
 func (s Server) handle(clientAddr string, rrq ReadReq) {
 	log.Printf("[%s] requested file: %s", clientAddr, rrq.Filename)
 
+	// 클라이언트와의 연결
 	conn, err := net.Dial("udp", clientAddr)
 	if err != nil {
 		log.Printf("[%s] dial: %v", clientAddr, err)
@@ -76,14 +83,18 @@ func (s Server) handle(clientAddr string, rrq ReadReq) {
 	defer func() { _ = conn.Close() }()
 
 	var (
-		ackPkt  Ack
-		errPkt  Err
+		ackPkt Ack
+		errPkt Err
+		// 전송하고자 하는 데이터 객체
 		dataPkt = Data{Payload: bytes.NewReader(s.Payload)}
 		buf     = make([]byte, DatagramSize)
 	)
 
+	// 패킷을 전부 수신하기 위한 for 문
 NEXTPACKET:
+	// 512 Bytes
 	for n := DatagramSize; n == DatagramSize; {
+		// 데이터 전송을 위한
 		data, err := dataPkt.MarshalBinary()
 		if err != nil {
 			log.Printf("[%s] preparing data packet: %v", clientAddr, err)
@@ -92,6 +103,8 @@ NEXTPACKET:
 
 	RETRY:
 		for i := s.Retries; i > 0; i-- {
+			// 데이터 전송
+			// 즉, 데이터 블록 패킷 1번 전송 후 확인 패킷 대기하고 다시 전송하는 루틴
 			n, err = conn.Write(data) // send the data packet
 			if err != nil {
 				log.Printf("[%s] write: %v", clientAddr, err)
@@ -99,11 +112,13 @@ NEXTPACKET:
 			}
 
 			// wait for the client's ACK packet
+			// 클라이언트의 ack 패킷 대기
 			_ = conn.SetReadDeadline(time.Now().Add(s.Timeout))
 
 			_, err = conn.Read(buf)
 			if err != nil {
 				if nErr, ok := err.(net.Error); ok && nErr.Timeout() {
+					// 에러 발생 시 RETRY 라벨의 루틴만 continue
 					continue RETRY
 				}
 
@@ -112,11 +127,13 @@ NEXTPACKET:
 			}
 
 			switch {
+			// 데이터 전송에 대한 응답 패킷 수신
 			case ackPkt.UnmarshalBinary(buf) == nil:
 				if uint16(ackPkt) == dataPkt.Block {
 					// received ACK; send next data packet
 					continue NEXTPACKET
 				}
+				// 에러 패킷 수신
 			case errPkt.UnmarshalBinary(buf) == nil:
 				log.Printf("[%s] received error: %v",
 					clientAddr, errPkt.Message)
